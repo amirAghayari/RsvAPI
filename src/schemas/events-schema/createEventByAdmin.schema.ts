cfbc38b907a37f302245762773234c5884c52858
amirAghayari <amiraghayari2119@gmail.com>
2026-06-30 13:05:50 +0330

    update events schema

--- src/schemas/events-schema/createEventByAdmin.schema.ts
+++ src/schemas/events-schema/createEventByAdmin.schema.ts
@@ -1,12 +1,19 @@
 import z from "zod";
 
-// TODO
 export const createEventByAdminSchema = z.object({
+  userId: z.string().min(1, "User ID is required"),
+
   title: z
     .string()
-    .min(4, "Event name required ")
-    .max(50, "The name cannot be longer than 50 characters."),
+    .min(4, "Event title required ")
+    .max(50, "The title cannot be longer than 50 characters."),
+  price: z
+    .number("The price of a numeric field is")
+    .min(1, "The price is required"),
+  location: z.string().min(4, "Event location required "),
   capacity: z.number("The capacity of a numeric field is").min(5),
+  remainingCapacity: z.number("The capacity of a numeric field is").max(30),
   executionDate: z.date(),
   salesStartTime: z.date(),
+  salesEndTime: z.date(),
 });
--- src/schemas/events-schema/updateEventByAdmin.schema.ts
+++ src/schemas/events-schema/updateEventByAdmin.schema.ts
@@ -4,13 +4,23 @@ import z from "zod";
 
 export const updateEventByAdminSchema = z.object({
   body: z.object({
-    name: z
+    userId: z.string().min(1, "User ID is required"),
+    // TODO : look this
+    status: z.string(
+      "status must be draft , published , canceled , finished or sold_out",
+    ),
+    title: z
       .string()
-      .min(4, "Event name required ")
-      .max(50, "The name cannot be longer than 50 characters."),
-    totalCapacity: z.number("The capacity of a numeric field is").min(5),
+      .min(4, "Event title required ")
+      .max(50, "The title cannot be longer than 50 characters."),
+    price: z
+      .number("The price of a numeric field is")
+      .min(1, "The price is required"),
+    location: z.string().min(4, "Event location required "),
+    capacity: z.number("The capacity of a numeric field is").min(5),
     executionDate: z.date(),
     salesStartTime: z.date(),
+    salesEndTime: z.date(),
   }),
   params: z.object({
     id: z.string().min(1, "Event ID is required"),
