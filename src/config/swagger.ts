5b66af4bd7e843e0739f1b7ee79adbc9cd0d566a
amirAghayari <amiraghayari2119@gmail.com>
2026-07-17 17:57:27 +0330

    add event swagger docs

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,8 +2,37 @@ import fs from "fs";
 import path from "path";
 import yaml from "yaml";
 
-const swaggerPath = path.resolve(__dirname, "../../docs/user-auth-openapi.yaml");
-const swaggerFile = fs.readFileSync(swaggerPath, "utf8");
-const swaggerSpec = yaml.parse(swaggerFile);
+const userAuthSwaggerPath = path.resolve(
+  __dirname,
+  "../../docs/user-auth-openapi.yaml",
+);
+const eventSwaggerPath = path.resolve(__dirname, "../../docs/event-openapi.yaml");
+
+const userAuthSwaggerFile = fs.readFileSync(userAuthSwaggerPath, "utf8");
+const eventSwaggerFile = fs.readFileSync(eventSwaggerPath, "utf8");
+
+const userAuthSwaggerSpec = yaml.parse(userAuthSwaggerFile);
+const eventSwaggerSpec = yaml.parse(eventSwaggerFile);
+
+const swaggerSpec = {
+  ...userAuthSwaggerSpec,
+  ...eventSwaggerSpec,
+  paths: {
+    ...(userAuthSwaggerSpec.paths || {}),
+    ...(eventSwaggerSpec.paths || {}),
+  },
+  components: {
+    ...(userAuthSwaggerSpec.components || {}),
+    ...(eventSwaggerSpec.components || {}),
+    securitySchemes: {
+      ...(userAuthSwaggerSpec.components?.securitySchemes || {}),
+      ...(eventSwaggerSpec.components?.securitySchemes || {}),
+    },
+    schemas: {
+      ...(userAuthSwaggerSpec.components?.schemas || {}),
+      ...(eventSwaggerSpec.components?.schemas || {}),
+    },
+  },
+};
 
 export default swaggerSpec;
